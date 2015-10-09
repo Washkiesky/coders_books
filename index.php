6b8d9c394fd11f4e4c09434ab3994852a5811bdc
Washkiesky <daniel.waszkiewicz@gmail.com>
2015-10-09 11:13:53 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <!-- dodanie skryptu do normalizacji pracy na innych pragladarkach-->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
+    <!-- bootstrap css-->
+    <link rel="stylesheet" media ="screen" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!--fontawesome-->
+    <link rel ="steelsheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!--podpisnamy css-->
+    <link rel = "stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+<!-- podpiecie skryptow z jq,bs i pliku js-->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
+
+
+
+
+<?php
+
+
+
